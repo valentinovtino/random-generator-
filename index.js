9d845e3072e610845af898f2b10ed66f47986e51
Valentino Valentin <valentinovalentine95@gmail.com>
2017-12-12 10:55:50 -0700

    adding letters

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const generateRandomNumbers = require('./lib/gen-random-numbers.js');
 const generateRandomLetters = require('./lib/gen-random-letters.js');
 
 module.exports = {
-  generateRandomNumbers
+  generateRandomNumbers,
   generateRandomLetters
 
 }
