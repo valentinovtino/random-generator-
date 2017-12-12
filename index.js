7ba9d98a1edf978248e2afd8da6cbb56e65bf4d4
Valentino Valentin <valentinovalentine95@gmail.com>
2017-12-12 10:47:26 -0700

    added random letter gan

---
+++ index.js
@@ -0,0 +1,8 @@
+const generateRandomNumbers = require('./lib/gen-random-numbers.js');
+const generateRandomLetters = require('./lib/gen-random-letters.js');
+
+module.exports = {
+  generateRandomNumbers
+  generateRandomLetters
+
+}
